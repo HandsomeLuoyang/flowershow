58a35cec0f01f5aefc3d07e387357e7e2073e589
yannicklei <yannicklei@tencent.com>
2024-09-10 14:45:00 +0800

    样式修改

--- content/config.mjs
+++ content/config.mjs
@@ -2,4 +2,20 @@
 /**
  * @type {import("../config/siteConfig").UserConfig}
  */
-export default {};
+
+const config = {
+    title: "Hsluoyang's Blog",
+    description: "",
+    defaultAuthor: "Hsluoyang",
+    domain: "",
+    showSidebar: true,
+    // 链接到你想要显示的网页，这会显示在导航栏和页脚
+    navLinks: [{ href: "/about", name: "About" }],
+    // 搜索功能
+    search: {
+        provider: "kbar",
+    }
+    };
+
+
+export default config;
